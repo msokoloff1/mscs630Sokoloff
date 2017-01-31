f4342606bcc789b8b89336eeae7a9d030b946add
msokoloff1 <matthewsokoloff@gmail.com>
2017-01-31 18:55:32 -0500

    Completed the first part of lab2

---
+++ labs/lab2/EuclideanAlgorithm.java
@@ -0,0 +1,34 @@
+/*
+*Matt Sokoloff
+*MSCS630
+*Lab 2
+*
+*This program applies the euclidean algorithm to a given input.
+*
+*/
+
+import java.util.Scanner;
+
+public class EuclideanAlgorithm {
+  
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    String line = ""; 
+    String[] numbers;
+    while(input.hasNext()) {
+      line = input.nextLine();
+      numbers = line.split(" ");
+      assert numbers.length == 2 : "Numbers of elements per line is not 2";
+      System.out.println(EuclideanAlgorithm.euclidAlgExt(Long.parseLong(numbers[0]),Long.parseLong(numbers[1])));   
+    }  
+
+
+  }
+
+  public static long euclidAlgExt(long a, long b) {
+    if(b == 0)
+      return a;
+
+    return EuclideanAlgorithm.euclidAlgExt(b, a%b);
+  }
+}  
