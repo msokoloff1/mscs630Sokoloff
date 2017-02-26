211d2cf6612b0e8762691544d402f154e9115f3d
msokoloff1 <matthewsokoloff@gmail.com>
2017-02-26 14:58:13 -0500

    Completed, formatted, tested.

--- labs/lab3/Determinant.java
+++ labs/lab3/Determinant.java
@@ -1,123 +1,89 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+
+/**
+ * @author matthewsokoloff (mscs630, spring 2017)
+ * This program can be used to calculate the determinant of 
+ * any arbitrarily sized square matrix in any modular number system.
+ * 
  */
-package faround;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
-/**
- *
- * @author matthewsokoloff
- */
 public class Determinant{
     
- 
-   
-  public static void main(String[] args) {
+  public static void main(String[] args) {      
     Scanner input = new Scanner(System.in);
     String line = "";
     String[] numbers;
-    //[row][column] //det = 1*(6-1) + all zeros = 5
-    int[][] test = new int[][]{{-1,0, 9, 8}
-                             , {6,7, 5, 6}
-                             , {4,9, 8, 7}
-                             , {8,9, 9, 9}};
+    ArrayList<String[]> lineData = new ArrayList<>();
     
-    
-    
-    int mod = 25;
-    ArrayList<int[][]> testing = new ArrayList<>();
-    testing.add(test);
-    
-    int finalValue = cofRecursive(10000,testing);
-    print("FINAL VALUE : " + finalValue);
-    
-    System.exit(0);
-    /*while(input.hasNext()) {
+    while(input.hasNext()) {
       line = input.nextLine();
       numbers = line.split(" ");
-      
+      lineData.add(numbers);
     }
-            */
+    
+    String[] line1 = lineData.get(0);
+    int mod = Integer.parseInt(line1[0]);
+    int size = Integer.parseInt(line1[1]);
+    int[][] matrix = new int[size][size];
+    
+    for (int lineIndex = 0; lineIndex < size; lineIndex++) {
+      String[] currentLine =  lineData.get(lineIndex+1);
+      for (int rowIndex = 0; rowIndex < size; rowIndex++) {
+        matrix[lineIndex][rowIndex] = Integer.parseInt(currentLine[rowIndex]);
+      }
+    }
+    
+    int finalValue = cofModDet(mod,matrix);
+    System.out.println("Original Matrix:");
+    System.out.println(Arrays.deepToString(matrix));
+    System.out.println("The determinant of your matrix in mod " + mod + " is : " + finalValue);
   }
 
   public static int cofModDet(int m, int[][] A) {
-    ArrayList<int[][]> cofs = new ArrayList<int[][]>();
+    ArrayList<int[][]> cofs = new ArrayList<>();
     cofs.add(A);
-    return Determinant.cofRecursive(m, cofs);
+    return (getDeterminant(cofs) % m);
   }
 
   public static int n2Det(int[][] A, int scaler) {
-      /*System.out.println("A[0][0] ---- " + A[0][0]);
-      System.out.println("A[1][1] ---- " + A[1][1]);
-      System.out.println("A[0][1] ---- " + A[0][1]);
-      System.out.println("A[1][0] ---- " + A[1][0]);
-*/
-      int result = (A[0][0] * A[1][1]) - (A[1][0] * A[0][1]);
-  //    print(scaler + "<= scaler");
-  //    print("Result " + result*scaler);
+    int result = (A[0][0] * A[1][1]) - (A[1][0] * A[0][1]);
     return result*scaler;
   }
 
-  public static int cofRecursive(int m, ArrayList<int[][]> A){
+  public static int getDeterminant(ArrayList<int[][]> A) {
     boolean reducing = true;
     int result = 0;
-    int everyOther = 0;
+    int scalerMultipleIndex = 0;
     boolean once = true;
-    
-    ArrayList<Integer> scalers = new ArrayList<Integer>();
-    ArrayList<Integer> resScalers = new ArrayList<Integer>();
-    
+    ArrayList<Integer> scalers = new ArrayList<>();
+    ArrayList<Integer> resScalers = new ArrayList<>();
     while(reducing){
-      ArrayList<int[][]> expanded = new ArrayList<int[][]>();
-      
-      
-      
-    //For each matrix, expand and add to expanded. Then return the result
+      ArrayList<int[][]> expanded = new ArrayList<>();
       int matrixIndex = 0;
       for(int[][] matrix : A) {
-          
-        if(matrix.length == 2){
-            reducing = false;
-         //   print("REDUCING:");
-            print("====");
-            print2(matrix);
-            print("====");
-            if(everyOther%2 == 0){
-                result += Determinant.n2Det(matrix, resScalers.get(everyOther));
-            }else{
-                result += Determinant.n2Det(matrix, resScalers.get(everyOther));
-            }
-            everyOther++;
-        }else{
-           
-        //Iterate over each column to get the scaler multiple and smaller matrix.
-        //Get the remainder (mod n)
-        for(int colIndex = 0; colIndex < matrix.length; colIndex++) {
-          int scaler =matrix[0][colIndex]* (int)Math.pow(-1, colIndex);
-          if(!once){
-              print(resScalers.size() + " " + matrixIndex);
+        if(matrix.length == 2) {
+          reducing = false;
+          result += n2Det(matrix, resScalers.get(scalerMultipleIndex));
+          scalerMultipleIndex++;
+        }else {
+          for(int colIndex = 0; colIndex < matrix.length; colIndex++) {
+            int scaler =matrix[0][colIndex]* (int)Math.pow(-1, colIndex);
+            if(!once) {
               scaler = resScalers.get(matrixIndex)*scaler;
-          }
-          
-          
-          
-          int[][] newMatrix = new int[matrix.length-1][matrix.length-1];
-            //Iterate over all columns again. Do not include the current column
+            }
+            int[][] newMatrix = new int[matrix.length-1][matrix.length-1];
             int count = 0;
             for(int colIndexNest = 0; colIndexNest < matrix.length; colIndexNest++) {
-                if(colIndexNest != colIndex){        
-                    int[] res = Determinant.slice(1, matrix.length-1, matrix, colIndexNest);
-                    newMatrix[count] = res;
-                    count++;
-                }
+              if(colIndexNest != colIndex) {        
+                int[] res = Determinant.slice(1, matrix.length-1, matrix, colIndexNest);
+                newMatrix[count] = res;
+                count++;
+              }
             }
-            expanded.add(Determinant.modScalerMatMul(newMatrix, m));
+            expanded.add(newMatrix);
             scalers.add(scaler);
           }
         }
@@ -127,56 +93,27 @@ public class Determinant{
       matrixIndex = 0;
       A = expanded;
       expanded = new ArrayList<>();
-      print(" " + scalers.size());
       resScalers = copyList(scalers);
-      scalers = new ArrayList<Integer>();
+      scalers = new ArrayList<>();
     }
     return result;
   }
   
-  
-  public static ArrayList<Integer> copyList(ArrayList<Integer> list){
-      ArrayList<Integer> copy = new ArrayList<Integer>();
-    for (Integer foo: list) {
-    copy.add(foo);
-    }
+  public static ArrayList<Integer> copyList(ArrayList<Integer> list) {
+    ArrayList<Integer> copy = new ArrayList<>();
+    list.stream().forEach((foo) -> {
+      copy.add(foo);
+    });
     return copy;
   }
-  
-  public static int[][] modScalerMatMul(int[][] matrix,int mod){
-      for(int row=0; row<matrix.length; row++){
-          for(int col = 0; col < matrix[0].length; col++){
-              matrix[row][col] = ((matrix[row][col])%mod);
-          }
-      }
-      
-      return matrix;
-    }
-  
+ 
   public static int[] slice(int start, int end, int[][] array, int column) {
-      int range = (end) - start;
-      int[] result = new int[range+1];
-      
-      for (int index = 0; index< range + 1; index++){
-          result[index] = array[start+index][column];
-      }
-      return result;
-      
+    int range = (end) - start;
+    int[] result = new int[range+1]; 
+    
+    for (int index = 0; index< range + 1; index++) {
+      result[index] = array[start+index][column];
+    }   
+    return result;  
   }
-   
-  
-  public static void print2(int[][] array){
-      System.out.println(Arrays.deepToString(array));
-  }
-  
-  public static void print1(int[] array){
-      System.out.println(Arrays.toString(array));
-  }
-  
-  public static void print(String s){
-      System.out.println(s);
-  }
-  
-
-}  
-
+}
