2c30eebf249bd931687be87f0238d52dfff9f96a
msokoloff1 <matthewsokoloff@gmail.com>
2017-04-02 12:42:24 -0400

    Debugged, tested, commented

--- labs/lab4/AESCipher.java
+++ labs/lab4/AESCipher.java
@@ -1,12 +1,14 @@
-import java.util.Arrays;
 
-/**
- *
- * @author matthewsokoloff
+/*
+ * @author Matthew Sokoloff
+ * AES, Lab 4, MSCS630
+ * AESCipher Class
  */
+
+
 public class AESCipher {
     
-     private static final String[][] S_BOX = {
+  private static final String[][] S_BOX = {
    { "63", "7C", "77", "7B", "F2", "6B", "6F", "C5", "30", "01", "67", "2B", "FE", "D7", "AB", "76" },
    { "CA", "82", "C9", "7D", "FA", "59", "47", "F0", "AD", "D4", "A2", "AF", "9C", "A4", "72", "C0" },
    { "B7", "FD", "93", "26", "36", "3F", "F7", "CC", "34", "A5", "E5", "F1", "71", "D8", "31", "15" },
@@ -22,9 +24,10 @@ public class AESCipher {
    { "BA", "78", "25", "2E", "1C", "A6", "B4", "C6", "E8", "DD", "74", "1F", "4B", "BD", "8B", "8A" },
    { "70", "3E", "B5", "66", "48", "03", "F6", "0E", "61", "35", "57", "B9", "86", "C1", "1D", "9E" },
    { "E1", "F8", "98", "11", "69", "D9", "8E", "94", "9B", "1E", "87", "E9", "CE", "55", "28", "DF" },
-   { "8C", "A1", "89", "0D", "BF", "E6", "42", "68", "41", "99", "2D", "0F", "B0", "54", "BB", "16" } };
+   { "8C", "A1", "89", "0D", "BF", "E6", "42", "68", "41", "99", "2D", "0F", "B0", "54", "BB", "16" } 
+  };
 
- private static final String[][] R_CON = {
+  private static final String[][] R_CON = {
    { "8D", "01", "02", "04", "08", "10", "20", "40", "80", "1B", "36", "6C", "D8", "AB", "4D", "9A" },
    { "2F", "5E", "BC", "63", "C6", "97", "35", "6A", "D4", "B3", "7D", "FA", "EF", "C5", "91", "39" },
    { "72", "E4", "D3", "BD", "61", "C2", "9F", "25", "4A", "94", "33", "66", "CC", "83", "1D", "3A" },
@@ -40,105 +43,126 @@ public class AESCipher {
    { "25", "4A", "94", "33", "66", "CC", "83", "1D", "3A", "74", "E8", "CB", "8D", "01", "02", "04" },
    { "08", "10", "20", "40", "80", "1B", "36", "6C", "D8", "AB", "4D", "9A", "2F", "5E", "BC", "63" },
    { "C6", "97", "35", "6A", "D4", "B3", "7D", "FA", "EF", "C5", "91", "39", "72", "E4", "D3", "BD" },
-   { "61", "C2", "9F", "25", "4A", "94", "33", "66", "CC", "83", "1D", "3A", "74", "E8", "CB", "8D" } };
+   { "61", "C2", "9F", "25", "4A", "94", "33", "66", "CC", "83", "1D", "3A", "74", "E8", "CB", "8D" } 
+  };
     
-
-    public static String aesRoundKeys(String keyHex) {
-        int NUM_NIBBLES = 2;
-        int squareMatrixDims = (int) Math.sqrt(keyHex.length()/NUM_NIBBLES);
-        String[][] keyMatrix = make2D(keyHex, squareMatrixDims);
-        String[][] wMatrix = makeWMatrix(keyMatrix);
-        System.out.println("Building result");
-        String result = "";
-        int count = 0;
-        for(int box = 0; box<11; box++){
-            for(int row = 0; row< 4; row++){
-                for(int col =(box*4); col<(box*4)+4; col++){
-                    System.out.println(col);
-                    result += wMatrix[row][col];
-                }
-            }
-            result +="\n";
+  /*
+  *@param keyHex : 16 2-digit hex numbers
+  *Returns a string contianing 11 keys composed of 16 hex numbers
+  */
+  public static String aesRoundKeys(String keyHex) {
+    int NUM_NIBBLES = 2;
+    int squareMatrixDims = (int) Math.sqrt(keyHex.length()/NUM_NIBBLES);
+    String[][] keyMatrix = make2D(keyHex, squareMatrixDims);
+    String[][] wMatrix = makeWMatrix(keyMatrix);
+    String result = "";
+    int count = 0;
+    for(int box = 0; box<11; box++) {
+      for(int col =(box*4); col<(box*4)+4; col++) {
+        for(int row = 0; row< 4; row++) {
+          result += wMatrix[row][col].toUpperCase();
         }
-        
-        
-        return result;
-
+      }
+      result +="\n";
     }
+    return result;
+  }
     
-    
-    private static String[][] make2D(String keyHex, int squareMatrixDims){
-        
-        String[][] keyMatrix = new String[squareMatrixDims][squareMatrixDims];
-        int row = 0, col = 0;
-        for (int index = 0; index < keyHex.length(); index += 2){
-            keyMatrix[row][col] = "" + keyHex.charAt(index) + keyHex.charAt(index+1);
-            col++;
-            if(col == 4){
-                col = 0;
-                row++;
-            }
-        }
-       return keyMatrix;
+  /*
+  *@param keyHex : 16 2-digit hex numbers
+  *@squareMatrixDims : x and y dimension for one round's key
+  *Returns a two dimensional array of the input key
+  */
+  private static String[][] make2D(String keyHex, int squareMatrixDims) {
+    String[][] keyMatrix = new String[squareMatrixDims][squareMatrixDims];
+    int row = 0, col = 0;
+    for (int index = 0; index < keyHex.length(); index += 2) {
+      keyMatrix[row][col] = "" + keyHex.charAt(index) + keyHex.charAt(index+1);
+      row++;
+      if(row == 4) {
+        row = 0;
+        col++;
+      }
     }
+    return keyMatrix;
+  }
     
-    
-    private static String[][] makeWMatrix(String[][] keyMatrix){
-        
-        int NUM_ROUNDS = 11;
-        String[][] wMatrix = new String[4][NUM_ROUNDS*4];
-        //Set round zero
-        for (int r = 0; r < 4; r++){
-            
-            System.arraycopy(keyMatrix[r], 0, wMatrix[r], 0, 4);
-        }
-        //All subsequent rounds
-        for(int j = 4; j <44; j++ ){
-            if(j%4 != 0){
-                for(int row = 0; row < 4; row ++){
-                    wMatrix[row][j] = xOr(wMatrix[row][j-4], wMatrix[row][j-1]);
-                }
-            }else{
-                String[] wNew = new String[4];
-                for(int row = 0; row < 4; row++){
-                    //Shift in place
-                    wNew[((row-1)+4)%4] = xOr(aesRcon(j), aesSBox(wMatrix[row][j-1]));                    
-                }   
-                for(int row = 0; row< 4; row++){
-                    wMatrix[row][j] = xOr(wMatrix[row][j-4], wNew[row]);
-                }
-            }
-        }
-        return wMatrix;
+  /*
+  *@param keyMatrix : 16 2-digit hex numbers, in a 4x4 array
+  *Returns a two dimensional array of containing all 11 keys
+  */
+  private static String[][] makeWMatrix(String[][] keyMatrix) {
+    int NUM_ROUNDS = 11;
+    String[][] wMatrix = new String[4][NUM_ROUNDS*4];
+    //Set round zero
+    for (int r = 0; r < 4; r++) {
+      System.arraycopy(keyMatrix[r], 0, wMatrix[r], 0, 4);
     }
+    //All subsequent rounds
+    for(int j = 4; j <44; j++ ) {
+      if(j%4 != 0) {
+        for(int row = 0; row < 4; row ++) {
+          wMatrix[row][j] = xOr(wMatrix[row][j-4], wMatrix[row][j-1]);
+          }
+      }else {
+        String[] wNew = new String[4];
+        for(int row = 0; row < 4; row++) {
+          //Shift in place
+          wNew[((row-1)+4)%4] = aesSBox(wMatrix[row][j-1]);                    
+        }   
+        String rconVal = aesRcon(j);
+        wNew[0] = xOr(rconVal, wNew[0]);
+     
+        for(int row = 0; row< 4; row++) {
+          wMatrix[row][j] = xOr(wMatrix[row][j-4], wNew[row]);
+        }
+      }
+    }
+    return wMatrix;
+  }
     
     
-    
-    private static String aesSBox(String inHex) {
+  /*
+  *@param inHex: 2-digit hex number
+  *returns the s-box value whose row index corresponds to the first digit
+  *  of the hex number and column whose index corresponds to 
+  *   the second digit of the hex number.
+  */
+  private static String aesSBox(String inHex) {
     String formatted = String.format("%2s", inHex).replace(' ', '0');
     int rowIndex = Integer.parseInt(""+formatted.charAt(0), 16);
     int colIndex = Integer.parseInt(""+formatted.charAt(1), 16);
     return S_BOX[rowIndex][colIndex];
-    }
+  }
     
-    private static String aesRcon(int round) {
+  /*
+  *@param round : The index number for the key currently being produced
+  *Returns the correct value of the r-con array depending on the round.
+  */
+  private static String aesRcon(int round) {
     return R_CON[0][round / 4];
  }
-    
-    private static String xOr(String value1, String value2){
-        int value1Decimal = Integer.parseInt(value1, 16);
-        int value2Decimal = Integer.parseInt(value2, 16);
-        String bin1 = String.format("%8s", Integer.toBinaryString(value1Decimal)).replace(' ', '0');
-        String bin2 = String.format("%8s", Integer.toBinaryString(value2Decimal)).replace(' ', '0');
-        StringBuilder sb = new StringBuilder();
-        for(int index = 0; index < bin1.length(); index ++ ){
-            sb.append((bin1.charAt(index) !=  bin2.charAt(index)) ? 1 : 0);
-        }
-        
-        int decimal = Integer.parseInt(sb.toString(),2);
-        String hexStr = Integer.toString(decimal,16);
-        return hexStr + "";
+  
+  
+  
+  /*
+  *@param value1 : 2-digit Hex number
+  *@param value2 : 2-digit Hex number
+  *Returns the result of applying the exlusive or to both inputs
+  */  
+  private static String xOr(String value1, String value2) {
+    int value1Decimal = Integer.parseInt(value1, 16);
+    int value2Decimal = Integer.parseInt(value2, 16);
+    String bin1 = String.format("%8s", Integer.toBinaryString(value1Decimal)).replace(' ', '0');
+    String bin2 = String.format("%8s", Integer.toBinaryString(value2Decimal)).replace(' ', '0');
+    StringBuilder sb = new StringBuilder();
+    for(int index = 0; index < bin1.length(); index ++ ) {
+      sb.append((bin1.charAt(index) !=  bin2.charAt(index)) ? 1 : 0);
     }
+    int decimal = Integer.parseInt(sb.toString(),2);
+    String hexStr = Integer.toString(decimal,16);
+    return hexStr + "";
+  }
     
 }
 
--- labs/lab4/DriverAES.java
+++ labs/lab4/DriverAES.java
@@ -1,19 +1,28 @@
-import java.util.Arrays;
+/*
+ * @author Matthew Sokoloff
+ * AES, Lab 4, MSCS630
+ * DriverAES Class
+ */
+
+
 import java.util.Scanner;
 
-/**
- *
- * @author matthewsokoloff
- */
-public class DriverAES {
+/*
+*Reads in 16 2-digit hex numbers from an external source
+*Then prints out the resulting 11 keys composed of 16 2-digit hex numbers
+*/
 
-    public static void main(String[] args) {
-        /*Scanner input = new Scanner(System.in);
-        String keyHex = input.nextLine().toUpperCase();
-        arrayPrint(AESCipher.aesRoundKeys(keyHex));*/
-        String keyHex = "5468617473206D79204B756E67204675";   
-        String result = AESCipher.aesRoundKeys(keyHex);
-        System.out.println(result);
-                
-    } 
+public class DriverAES {
+    
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    String keyHex = input.nextLine().toUpperCase();
+    long startTime = System.nanoTime();
+    System.out.println("Hex Input: \n" + keyHex );
+    System.out.println("\n11 Resulting Keys: \n" + AESCipher.aesRoundKeys(keyHex));
+    long elapsedInMS = (System.nanoTime() - startTime)/ 1000000;
+    System.out.println("\nElapsed Time : " + elapsedInMS +" ms");          
+  } 
+  
 }
+
